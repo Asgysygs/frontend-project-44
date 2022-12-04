eb78b8d3a3df977a04b57d7f20a621f80eaa9d34
Asgysygs <97634666+Asgysygs@users.noreply.github.com>
2022-12-04 23:32:31 +0300

    stefourt

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
-
-
 import readlineSync from 'readline-sync';
 
 export const getUserName = () => {
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
 };
+
+
+
+
