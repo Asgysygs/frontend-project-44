3e497cbb0b90cf546793b695eefd8dc5dde9ad37
Your Name <you@example.com>
2023-03-13 15:12:18 +0300

    AD

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,3 @@ import readlineSync from 'readline-sync';
 
 export const getAnswerForQuestion = (question) => readlineSync.question(`${question} `);
 export const getUserName = () => getAnswerForQuestion('May I have your name?');
-
-
-
